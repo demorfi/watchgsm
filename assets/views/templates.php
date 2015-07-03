f5060b6829319dca1b20fff78bb7deb278dfba09
demorfi <demorfi@gmail.com>
2015-07-03 07:46:00 +0600

    Updated view sms templates

--- assets/views/templates.php
+++ assets/views/templates.php
@@ -1,7 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: el
- * Date: 07.05.15
- * Time: 0:48
- */
+<form method="POST" role="form">
+    <div class="panel panel-default">
+        <div class="panel-heading">Total:</div>
+        <div class="table-responsive">
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
+                    <th width="2%">#</th>
+                    <th width="10%">To</th>
+                    <th width="88%">Text</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+        <div class="panel-footer">
+            <button type="submit" name="delete" class="btn btn-danger">Delete</button>
+            <button type="submit" name="send" class="btn btn-primary">Send</button>
+        </div>
+    </div>
+</form>
