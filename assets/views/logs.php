6fd086b4d5870d1bcac429571c1652b55904e631
demorfi <demorfi@gmail.com>
2015-08-05 03:22:00 +0600

    Fix methods names for logs page

--- assets/views/logs.php
+++ assets/views/logs.php
@@ -1,17 +1,17 @@
 <ul class="nav nav-pills">
-    <li class="active <?php echo empty($logDaemon) ? 'disabled' : ''; ?>">
+    <li class="active <?php echo empty($log_daemon) ? 'disabled' : ''; ?>">
         <a href="#daemon" data-toggle="pill">Daemon</a>
     </li>
-    <li class="<?php echo empty($logRegularRun) ? 'disabled' : ''; ?>">
+    <li class="<?php echo empty($log_regular_run) ? 'disabled' : ''; ?>">
         <a href="#regular-run" data-toggle="pill">Regular run <?php echo $device; ?></a>
     </li>
 </ul>
 
 <div class="tab-content navbar-btn">
     <div class="tab-pane active" id="daemon">
-        <div class="well"><?php echo nl2br($logDaemon); ?></div>
+        <div class="well"><?php echo nl2br($log_daemon); ?></div>
     </div>
     <div class="tab-pane" id="regular-run">
-        <div class="well"><?php echo nl2br($logRegularRun); ?></div>
+        <div class="well"><?php echo nl2br($log_regular_run); ?></div>
     </div>
 </div>
--- classes/App/Controller/Logs.php
+++ classes/App/Controller/Logs.php
@@ -7,18 +7,18 @@ class Logs extends \App\Controller\App
 
     public function action_index()
     {
-        $this->view->title         = 'Logs';
-        $this->view->logDaemon     = '';
-        $this->view->logRegularRun = '';
+        $this->view->title           = 'Logs';
+        $this->view->log_daemon      = '';
+        $this->view->log_regular_run = '';
 
-        $logPath = $this->getHelper()->getConfigVariable('logfile');
-        if (is_readable($logPath)) {
-            $this->view->logDaemon = trim(htmlspecialchars(file_get_contents($logPath)));
+        $log_path = $this->pixie->get_smstools_var('logfile');
+        if (is_readable($log_path)) {
+            $this->view->log_daemon = trim(htmlspecialchars(file_get_contents($log_path)));
         }
 
-        $device = $this->getHelper()->getConfigVariable($this->getHelper()->getConfigVariable('devices'));
+        $device = $this->pixie->get_smstools_var($this->pixie->get_smstools_var('devices'));
         if (isset($device['regular_run_logfile']) && is_readable($device['regular_run_logfile'])) {
-            $this->view->logRegularRun = trim(htmlspecialchars(file_get_contents($device['regular_run_logfile'])));
+            $this->view->log_regular_run = trim(htmlspecialchars(file_get_contents($device['regular_run_logfile'])));
         }
     }
 
