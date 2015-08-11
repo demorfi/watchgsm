736a2bbfab846b1e2943a72a5ea2ba4b4ec7788e
demorfi <demorfi@gmail.com>
2015-08-11 16:07:02 +0600

    Added cleaning logs

--- assets/views/logs.php
+++ assets/views/logs.php
@@ -1,17 +1,52 @@
-<ul class="nav nav-pills">
-    <li class="active <?php echo empty($log_daemon) ? 'disabled' : ''; ?>">
-        <a href="#daemon" data-toggle="pill">Daemon</a>
+<ul class="list-inline">
+
+    <!-- daemon logs -->
+    <li>
+        <fieldset <?php echo empty($log_daemon) ? ' disabled ' : ''; ?>>
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary" data-target="#daemon" data-toggle="pill">Daemon</button>
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                    <span class="caret"></span>
+                    <span class="sr-only">Options</span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="<?php echo $this->pixie->router->get('default')->url(
+                            array('controller' => 'logs')
+                        ); ?>?clear=daemon"><i class="fa fa-trash"></i> Clear log file</a>
+                    </li>
+                </ul>
+            </div>
+        </fieldset>
     </li>
-    <li class="<?php echo empty($log_regular_run) ? 'disabled' : ''; ?>">
-        <a href="#regular-run" data-toggle="pill">Regular run <?php echo $device; ?></a>
+
+    <!-- regular run logs -->
+    <li>
+        <fieldset <?php echo empty($log_regular_run) ? ' disabled ' : ''; ?>>
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary" data-target="#regular-run"
+                        data-toggle="pill">Regular run <?php echo $device; ?></button>
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                    <span class="caret"></span>
+                    <span class="sr-only">Options</span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="<?php echo $this->pixie->router->get('default')->url(
+                            array('controller' => 'logs')
+                        ); ?>?clear=regular-run"><i class="fa fa-trash"></i> Clear log file</a>
+                    </li>
+                </ul>
+            </div>
+        </fieldset>
     </li>
 </ul>
 
 <div class="tab-content navbar-btn">
     <div class="tab-pane active" id="daemon">
-        <div class="well"><?php echo nl2br($log_daemon); ?></div>
+        <div class="well"><?php echo empty($log_daemon) ? 'Empty' : nl2br($log_daemon); ?></div>
     </div>
     <div class="tab-pane" id="regular-run">
-        <div class="well"><?php echo nl2br($log_regular_run); ?></div>
+        <div class="well"><?php echo empty($log_regular_run) ? 'Empty' : nl2br($log_regular_run); ?></div>
     </div>
 </div>
--- classes/App/Controller/Logs.php
+++ classes/App/Controller/Logs.php
@@ -12,11 +12,29 @@ class Logs extends \App\Controller\App
         $this->view->log_regular_run = '';
 
         $log_path = $this->pixie->get_smstools_var('logfile');
+        $device   = $this->pixie->get_smstools_var($this->pixie->get_smstools_var('devices'));
+
+        // clear logs
+        if (($clear = $this->request->get('clear')) !== null) {
+            switch ($clear) {
+                case('daemon'):
+                    if (is_readable($log_path)) {
+                        file_put_contents($log_path, '');
+                    }
+                    break;
+                case('regular-run'):
+                    if (isset($device['regular_run_logfile']) && is_readable($device['regular_run_logfile'])) {
+                        file_put_contents($device['regular_run_logfile'], '');
+                    }
+                    break;
+            }
+            $this->add_message_success('Log file is cleared!');
+        }
+
         if (is_readable($log_path)) {
             $this->view->log_daemon = trim(htmlspecialchars(file_get_contents($log_path)));
         }
 
-        $device = $this->pixie->get_smstools_var($this->pixie->get_smstools_var('devices'));
         if (isset($device['regular_run_logfile']) && is_readable($device['regular_run_logfile'])) {
             $this->view->log_regular_run = trim(htmlspecialchars(file_get_contents($device['regular_run_logfile'])));
         }
